d4e4cac04f6ac6c9e8e1267dc8c83036934b5921
meglat <meganlathem@gmail.com>
2026-09-12 00:06:06 -0500

    Add thumbnail URL for 'Follow the Dream' book

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const lessons=[
       "author": "Peter Sís",
       "url": "https://www.penguinrandomhouse.com/books/168056/follow-the-dream-by-peter-sis/?utm_source=chatgpt.com",
       "description": "Discover the voyage that opened the Age of Exploration. This beautifully illustrated picture book helps children understand how Christopher Columbus's journey paved the way for later Spanish explorers who eventually reached Alabama's Gulf Coast."
+      "thumbnail": "https://books.google.com/books/content?id=F_wAOC8hl-sC&printsec=frontcover&img=1&zoom=1&source=gbs_api"
     },
     "video": {
       "title": "Delta Safari: The Forgotten Conquistador",
